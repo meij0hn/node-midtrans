0ec25ea52e89681240a66861012746aeafe5da0c
meij0hn <meijohnnura@gmail.com>
2021-05-11 10:24:56 +0700

    Pertama Upload
    Pertama Upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 2002
+
+const midtransClient = require('midtrans-client');
+// Create Core API / Snap instance (both have shared `transactions` methods)
+
+
+const apiClient = new midtransClient.Snap({
+        isProduction : true,
+        serverKey : 'Mid-server-5SjUm4DPCDKqf_PAMUT2Ht4E',
+        clientKey : 'Mid-client-MGKpnVja6krOzV0L'
+    });
+
+
+
+// get status of transaction that already recorded on midtrans (already `charge`-ed) 
+app.post('/mid/:ordercode', (req, res) => {
+    const id = req.params
+    apiClient.transaction.status(id.ordercode)
+        .then((response)=>{
+            console.log(response);
+            this.info = response;
+        })
+    // res.send(this.info)
+    res.send(this.info)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello, ini adalah isi dari data midtrans')
+})
+
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}')
+})
