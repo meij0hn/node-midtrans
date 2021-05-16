a47ac8e8ce0b5898d9b6ae080a70c669afec959a
meij0hn <meijohnnura@gmail.com>
2021-05-16 21:14:35 +0700

    20210516_02

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 const express = require('express')
-const app = express()
-const port = 3002
-
+const cors = require('cors')
 const midtransClient = require('midtrans-client');
 // Create Core API / Snap instance (both have shared `transactions` methods)
 
+const app = express()
+const port = 3002
 
-const apiClient = new midtransClient.Snap({
+
+
+
+const apiClienRH = new midtransClient.Snap({
         isProduction : true,
         serverKey : 'Mid-server-5SjUm4DPCDKqf_PAMUT2Ht4E',
         clientKey : 'Mid-client-MGKpnVja6krOzV0L'
     });
 
 
+app.use(cors())	
+	
 
 // get status of transaction that already recorded on midtrans (already `charge`-ed) 
 app.post('/api/midtrans/status/:ordercode', (req, res) => {
     const id = req.params
-    apiClient.transaction.status(id.ordercode)
+    apiClienRH.transaction.status(id.ordercode)
         .then((response)=>{
             console.log(response);
-            this.info = response;
+            this.statusinfo = response;
         })
-    res.send(this.info)
+    res.json(this.statusinfo)
 })
 
 app.post('/api/midtrans/expire/:ordercode', (req, res) => {
     const id = req.params
-    apiClient.transaction.expire(id.ordercode)
+    apiClienRH.transaction.expire(id.ordercode)
         .then((response)=>{
             console.log(response);
-            this.info = response;
+            this.expireinfo = response;
         })
-    res.send(this.info)
+    res.json(this.expireinfo)
 })
 
 app.get('/test', (req, res) => {
